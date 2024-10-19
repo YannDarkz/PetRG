6aa7bddf6b041147c861dd4f663eb9616f333722
yanndarkz <iansantosmcqueen@gmail.com>
2024-10-19 00:27:31 -0300

    feature main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+const config = {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideHttpClient()             
+  ]
+};
+
+bootstrapApplication(AppComponent, config)
   .catch((err) => console.error(err));
